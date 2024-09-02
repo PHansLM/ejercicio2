71e2f36e5ce7128392875f4103121adda67785df
Pablo <pablito.hans@hotmail.com>
2024-09-01 22:25:36 -0400

    feature/Sumar

---
+++ aritmetica.java
@@ -0,0 +1,11 @@
+public class aritmetica {
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    // Método principal para probar funciones
+    public static void main(String[] args) {
+        int resultado = suma(5, 3);
+        System.out.println("La suma de 5 y 3 es: " + resultado);
+    }
+}
