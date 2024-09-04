a1136bf20696d14201cb0ee063b7a7a1289b869f
FerL1021 <jimenezmolina0472@gmail.com>
2024-09-04 00:00:50 -0400

    feature/division

--- aritmetica.java
+++ aritmetica.java
@@ -7,12 +7,26 @@ public class aritmetica {
         return a * b;
     }
 
+    public static double division(int a, int b){
+        double res = 0.0;
+        if(b == 0){
+            System.out.println("No existe la division por cero");
+            res = Double.NaN; 
+        }else{
+            res = (double) a / b;
+        }
+        return res;
+        }
+
     // Método principal para probar funciones
     public static void main(String[] args) {
         int resultado = suma(5, 3);
         System.out.println("La suma de 5 y 3 es: " + resultado);
         int producto = multiplicacion(5,8);
         System.out.println("El producto de la multiplicacion es: " + producto);
+
+        double res = division(8,4);
+        System.out.println("el resultado de la division es: " + res);
     }
 
     
