65c7abf8fcea58c15b266e532399a490840dcc32
Nilson Huanca <zubietanilson@gmail.com>
2024-09-03 23:00:43 -0400

    feature/multiplicacion

--- aritmetica.java
+++ aritmetica.java
@@ -3,9 +3,17 @@ public class aritmetica {
         return a + b;
     }
 
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
     // Método principal para probar funciones
     public static void main(String[] args) {
         int resultado = suma(5, 3);
         System.out.println("La suma de 5 y 3 es: " + resultado);
+        int producto = multiplicacion(5*8);
+        System.out.println("El producto de la multiplicacion es: " + producto);
     }
+
+    
 }
